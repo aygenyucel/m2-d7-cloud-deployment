8b9dfda41ee3575eabd64a6fbb6bb26f57347395
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-01-12 17:36:09 +0300

    adds endpoints for export authors, blogposts as csv

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { pipeline } from "stream";
+import {
+  getAuthorsJsonReadableStream,
+  getBlogPostsJsonReadableStream,
+} from "../lib/fs-tools.js";
 import { getPdfReadableStream } from "../lib/pdf-tools.js";
+
+import json2csv from "json2csv";
 
 const filesRouter = express.Router();
 
 filesRouter.get("/pdf", (req, res, next) => {
-  console.log("jkdfhfjksdjf");
   res.setHeader("Content-Disposition", "attachment; filename=blogPost.pdf");
 
   const source = getPdfReadableStream();
@@ -13,6 +18,38 @@ filesRouter.get("/pdf", (req, res, next) => {
   pipeline(source, destination, (err) => {
     console.log(err);
   });
+});
+
+filesRouter.get("/blogPostsCSV", (req, res, next) => {
+  try {
+    res.setHeader("Content-Disposition", "attachment; filename=blogPosts.csv");
+    const source = getBlogPostsJsonReadableStream();
+    const transform = new json2csv.Transform();
+    const destination = res;
+
+    pipeline(source, transform, destination, (err) => {
+      if (err) console.log(err);
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+filesRouter.get("/authorsCSV", (req, res, next) => {
+  try {
+    res.setHeader("Content-Disposition", "attachment; filename=authors.csv");
+    const source = getAuthorsJsonReadableStream();
+    console.log(source);
+    const transform = new json2csv.Transform();
+    const destination = res;
+
+    pipeline(source, transform, destination, (err) => {
+      if (err) console.log(err);
+    });
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 });
 
 export default filesRouter;
